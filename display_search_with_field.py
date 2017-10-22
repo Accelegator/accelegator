86d2924f9172112026e33a8245cd47c6ce2d74c6
yeeunmariakim <yeeunmariakim@gmail.com>
2017-10-22 01:34:31 -0400

    adding starting file

---
+++ display_search_with_field.py
@@ -0,0 +1,5 @@
+""" Displays list of advisees with responses that match search query and field. """
+
+# Implementation will be moved to display.py once completed
+
+def display_search_with_field(list_of_tuples):
