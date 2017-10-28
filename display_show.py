7685cb913f16792b1effb4096a6b8de36c6053bf
lylet-AC <lylet@allegheny.edu>
2017-10-27 23:03:54 -0400

    added basic logic for display_show

--- display_show.py
+++ display_show.py
@@ -1,5 +1,10 @@
 """ Displays an advisee's flattened responses. """
 
 # Implementation will be moved to display.py once completed
-
 def display_show(tuple_of_tuples):
+
+	#for each tuple in the tuple of tuples, we'll print out the field, timestamp, and the response given
+	for tuple in test_tuple:
+		print(tuple[1])
+		print(tuple[0])
+		print(tuple[2])
