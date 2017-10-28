9cc2ffa8e5daa423a1211dc45c0b5a25ece261ca
yeeunmariakim <yeeunmariakim@gmail.com>
2017-10-28 18:29:13 -0400

    refactoring display_search and align functions

--- display_search.py
+++ display_search.py
@@ -1,7 +1,8 @@
 """ Displays list of advisees with responses that match search query. """
 # Implementation will be moved to display.py once completed
 
-from colors import *
+from colors import negative
+
 
 def display_search(list_of_tuples):
     """ Returns string of all responses returned by search """
@@ -17,25 +18,19 @@ def display_search(list_of_tuples):
         # if timestamp is latest, invert the colors
         timestamp_tuple = response[TIMESTAMP_TUPLE_INDEX]
         timestamp_boolean = timestamp_tuple[TIMESTAMP_BOOLEAN_INDEX]
-        formatted_timestamp = format_timestamp(timestamp_tuple, timestamp_boolean)
-        response_string = align(response[EMAIL_INDEX], formatted_timestamp, timestamp_boolean) + "\n" + response[FIELD_INDEX] + "\n" + response[RESPONSE_INDEX] + "\n\n"
+        # if timestamp is latest, invert its color
+        timestamp = negative(timestamp_tuple[TIMESTAMP_STRING_INDEX]) if timestamp_boolean else timestamp_tuple[TIMESTAMP_STRING_INDEX]
+
+        response_string = align(response[EMAIL_INDEX], timestamp, timestamp_boolean) + "\n" + response[FIELD_INDEX] + "\n" + response[RESPONSE_INDEX] + "\n\n"
         result += response_string
 
     return result
 
-def align(left, right, is_negative_timestamp):
+
+def align(left, right, is_negative_timestamp=False):
     """ Returns string with "left" aligned to the left and "right" aligned to the right """
     if is_negative_timestamp:
         # adjust right alignment for ansi sequence of inverted timestamp
         return "{:<40s}{:>48s}".format(left, right)
     else:
         return "{:<40s}{:>40s}".format(left, right)
-
-def format_timestamp(timestamp_tuple, is_latest):
-    # inverts the color of timestamp if it is the latest response
-    TIMESTAMP_STRING_INDEX = 0
-    TIMESTAMP_BOOLEAN_INDEX = 1
-    if (timestamp_tuple[TIMESTAMP_BOOLEAN_INDEX]):
-        return negative(timestamp_tuple[TIMESTAMP_STRING_INDEX])
-    else:
-        return timestamp_tuple[TIMESTAMP_STRING_INDEX]
