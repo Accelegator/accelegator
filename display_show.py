50349dc0fcda7fee6adc6c88aa85c3ba3963259d
lylet-AC <lylet@allegheny.edu>
2017-10-28 23:07:49 -0400

    added linewrapping for cmd lines

--- display_show.py
+++ display_show.py
@@ -5,5 +5,6 @@ def display_show(tuple_of_tuples):
 
 	#for each tuple in the tuple of tuples, we'll print out the field, timestamp, and the response given
 	for tuple in tuple_of_tuples:
-		print(textwrap.fill(tuple[1]), textwrap.fill(tuple[0]))
-		print(textwrap.fill(tuple[2]))
+		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
+		print(textwrap.fill(tuple[1], 80), textwrap.fill(tuple[0], 80))
+		print(textwrap.fill(tuple[2], 80))
