adbc87d0d9012fcebfa35b713982be3a3a83f60f
lylet-AC <lylet@allegheny.edu>
2017-10-28 22:59:53 -0400

    added textwrapping to output

--- display_show.py
+++ display_show.py
@@ -1,10 +1,9 @@
 """ Displays an advisee's flattened responses. """
-
+import textwrap
 # Implementation will be moved to display.py once completed
 def display_show(tuple_of_tuples):
 
 	#for each tuple in the tuple of tuples, we'll print out the field, timestamp, and the response given
-	for tuple in test_tuple:
-		print(tuple[1])
-		print(tuple[0])
-		print(tuple[2])
+	for tuple in tuple_of_tuples:
+		print(textwrap.fill(tuple[1]), textwrap.fill(tuple[0]))
+		print(textwrap.fill(tuple[2]))
