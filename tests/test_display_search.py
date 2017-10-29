ab89f55be819dcab09bbee90fdfd67a48a6d685e
yeeunmariakim <yeeunmariakim@gmail.com>
2017-10-28 20:17:46 -0400

    updating test cases to reflect heading print out

--- tests/test_display_search.py
+++ tests/test_display_search.py
@@ -4,17 +4,17 @@ from display_search import align
 
 
 def test_display_search_with_latest_timestamp():
-    list = [(("timestamp", True), "email", "field", "response")]
+    list = [(("timestamp", True), "keyword", "email", "field", "response")]
     result = display_search(list)
     num_of_spaces = 66
-    assert repr(result) == repr("email" + ' ' * num_of_spaces + "\x1b[7mtimestamp\x1b[0m\nfield\nresponse\n\n")
+    assert repr(result) == repr("Displaying search results for keyword \x1b[1mkeyword\x1b[0m\n\n" + "email" + ' ' * num_of_spaces + "\x1b[7mtimestamp\x1b[0m\nfield\nresponse\n\n")
 
 
 def test_display_search_with_non_latest_timestamp():
-    list = [(("timestamp", False), "email", "field", "response")]
+    list = [(("timestamp", False), "keyword", "email", "field", "response")]
     result = display_search(list)
     num_of_spaces = 66
-    assert repr(result) == repr("email" + ' ' * num_of_spaces + "timestamp\nfield\nresponse\n\n")
+    assert repr(result) == repr("Displaying search results for keyword \x1b[1mkeyword\x1b[0m\n\n" + "email" + ' ' * num_of_spaces + "timestamp\nfield\nresponse\n\n")
 
 
 def test_align_with_negative_timestamp():
