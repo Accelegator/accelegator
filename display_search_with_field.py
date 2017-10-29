d30b11797f5109c95978b402547c6b9498cda624
yeeunmariakim <yeeunmariakim@gmail.com>
2017-10-28 20:17:58 -0400

    adding display_search_with_field implementation

--- display_search_with_field.py
+++ display_search_with_field.py
@@ -1,5 +1,8 @@
 """ Displays list of advisees with responses that match search query and field. """
-
 # Implementation will be moved to display.py once completed
 
+from display_search import display_search
+
+
 def display_search_with_field(list_of_tuples):
+    return display_search(list_of_tuples, True)
