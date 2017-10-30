c7b2675efff762060cf62bcf32481cc4a835ab47
Karol Vargas <vargask@aldenv166.allegheny.edu>
2017-10-29 21:57:21 -0400

    last update

--- display_help.py
+++ display_help.py
@@ -3,6 +3,15 @@
 # Implementation will be moved to display.py once completed
 
 def display_help(help):
-    print("Commands\n --------------------- \nhelp <command>              display verbose descriptions of <command> and valid arguments\nshow <email>                   display flattened responses for advisee with <email>");
-
-return; 
+    print("Commands");
+    print("show <email>                   display flattened responses for advisee with <email>");
+    print("help <command>              display verbose descriptions of <command> and valid arguments");
+    print("help                        list commands & brief descriptions  ");
+    print("quit                         exit the program");
+    print("show <email> <field>         display responses for given field, incl. history");
+    print("list                         list advisees by emails, show latest response date");\
+    print("search <keyword>             search all fields of all responses of all students for keyword");
+    print("Search <field> <keyword>     ");
+    print("write command                write command output to file (prompt for filepath)");
+    
+return;
