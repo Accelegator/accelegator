f8ff6c8048d8b84ae4fd55174063d1d4bb545fd1
Karol Vargas <vargask@aldenv174.allegheny.edu>
2017-10-30 19:49:26 -0400

    refactored print statements and properly indented

--- display_help.py
+++ display_help.py
@@ -3,15 +3,14 @@
 # Implementation will be moved to display.py once completed
 
 def display_help(help):
-    Help = """Commands
-            show <email>                      display flattened responses for advisee with <email>
-            help <command>                    display verbose descriptions of <command> and valid arguments
-            help                              list commands & brief descriptions
-            quit                              exit the program
-            show <email> <field>              display responses for given field, incl. history
-            list                              list advisees by emails, show latest response date
-            search <keyword>                  search all fields of all responses of all students for keyword
-            Search <field> <keyword>          search given of all responses of all students for keyword
-            write command                     write command output to file (prompt for filepath)""";
 
-    return Help; 
+    return  "{:<40s}{:<48s}".format('Commands, descriptions');
+            "{:<40s}{:<48s}".format('quit, exit the program');
+            "{:<40s}{:<48s}".format('show <email>, display flattened responses for advisee with <email>');
+            "{:<40s}{:<48s}".format('help <command>, descriptions');
+            "{:<40s}{:<48s}".format('help, list commands & brief descriptions');
+            "{:<40s}{:<48s}".format('show <email> <field>, display responses for given field, incl. history');
+            "{:<40s}{:<48s}".format('list, list advisees by emails & shows latest response date');
+            "{:<40s}{:<48s}".format('search <keyword>, search all fields of all responses of all students for keyword');
+            "{:<40s}{:<48s}".format('search <field> <keyword>, search given of all responses of all students for keyword');
+            "{:<40s}{:<48s}".format('write command, write command output to file(prompt for filepath)');
