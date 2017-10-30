1320737e402ab3c33b1113ccfa0617feb1ad58b6
lylet-AC <lylet@allegheny.edu>
2017-10-30 11:24:27 -0400

    display_show returns a string and has constants for which tuple to output

--- display_show.py
+++ display_show.py
@@ -3,8 +3,20 @@ import textwrap
 # Implementation will be moved to display.py once completed
 def display_show(tuple_of_tuples):
 
+	#these constants control which portion of the tuple come first
+	field = 1
+	timestamp = 0
+	response = 2
+	string_tuple_of_tuples = ""
+	
 	#for each tuple in the tuple of tuples, we'll print out the field, timestamp, and the response given
 	for tuple in tuple_of_tuples:
 		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
-		print(textwrap.fill(tuple[1], 80), textwrap.fill(tuple[0], 80))
-		print(textwrap.fill(tuple[2], 80))
+		string_tuple_of_tuples+=(textwrap.fill(str(tuple[field]), 80))
+		string_tuple_of_tuples+=(textwrap.fill(str(tuple[timestamp]), 80))
+		string_tuple_of_tuples+=(textwrap.fill(str(tuple[response]), 80))
+		return string_tuple_of_tuples
+		
+		
+tuple_of_tuples = (("timestamp", "id", "12345"), ("timestamp", "email", "maria@maria.com"))
+print(display_show(tuple_of_tuples))
