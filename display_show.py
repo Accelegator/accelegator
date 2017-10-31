589fcd6ad9f276937fc381435d6501783ff860ff
lylet-AC <lylet@allegheny.edu>
2017-10-31 15:46:21 -0400

    fixed implementation of display_show_with_field.py for new inut type, fixed documentation a bit

--- display_show.py
+++ display_show.py
@@ -14,13 +14,13 @@ def display_show(result_tuple):
 		logging.error("No input was found for tuple_of_tuples")
 		return "No responses to display"
 		
-	#for display_show
+	#input for display_show
 	"""`show <email>` --- `(email, [((timestamp, latest), field, response)])`"""
 	
 	#CONSTANTS
 	EMAIL_INDEX = 0
 	RESPONSE_LIST = 1
-	FIELD_INDEX = 1 
+	FIELD_INDEX = 1
 	RESPONSE_INDEX = 2
 	TIMESTAMP_INDEX = 0
 	TIMESTAMP_STR_INDEX = 0
@@ -28,30 +28,28 @@ def display_show(result_tuple):
 
 	field_str = ""
 	timestamp_str= ""
-	string_tuple_of_tuples = ""
+	string_result_tuple = ""
 	
 	initial_string = textwrap.fill("Showing all results for: " + str(result_tuple[EMAIL_INDEX]), 80)
 	initial_string += "\n\n"
 	
 	string_tuple_of_tuples = initial_string
 	
-	"""Message for code inheriter"""
-	#TODO: iterate through response list
-	
+	#iterates through RESPONSE_LIST
 	for tuple in result_tuple[RESPONSE_LIST]:
-		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
 		field_str = (str(tuple[FIELD_INDEX]))
 		timestamp_tuple = tuple[TIMESTAMP_INDEX]
 		timestamp_str = str(timestamp_tuple[TIMESTAMP_STR_INDEX])
 		
 		if timestamp_tuple[TIMESTAMP_BOOL_INDEX]:
-			negative(timestamp_str)
+			timestamp_str = negative(timestamp_str)
 		
 		#align function will align text to the right and left side of the terminal
 		string_result_tuple += align(field_str, timestamp_str)
 		string_result_tuple +="\n"
 		
 		#no alignment needed for the RESPONSE
+		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.		
 		string_result_tuple += (textwrap.fill(str(tuple[RESPONSE_INDEX]), 80))
 		string_result_tuple += "\n \n"
 		
--- display_show_with_field.py
+++ display_show_with_field.py
@@ -1,6 +1,54 @@
-""" Displays list of advisees with fields that match search query and field. """
+""" Displays an advisee's flattened responses. """
+import textwrap
+import logging
+from colors import bold
+from colors import negative
 
-from display_show import display_show
 
 def display_show_with_field(tuple_of_tuples):
-    return display_show(tuple_of_tuples, True)
+	""" Displays an advisee's flattened responses. """
+	
+	#immediately leaves if the output is None
+	if not tuple_of_tuples:
+		logging.error("No input was found for tuple_of_tuples")
+		return "No responses to display"
+		
+	#input for display_show_with_feild
+	"""`show <email> <field>` --- `(email, field, [((timestamp, latest), response)])`"""
+	
+	#CONSTANTS
+	EMAIL_INDEX = 0
+	RESPONSE_LIST = 2
+	FIELD_INDEX = 1 
+	RESPONSE_INDEX = 1
+	TIMESTAMP_INDEX = 0
+	TIMESTAMP_STR_INDEX = 0
+	TIMESTAMP_BOOL_INDEX = 1
+
+	field_str = ""
+	timestamp_str= ""
+	string_tuple_of_tuples = ""
+	
+	initial_string = textwrap.fill("Displaying " + result_tuple[EMAIL_INDEX] + "'s results for: " + result_tuple[FIELD_INDEX], 80)
+	initial_string += "\n\n"
+	
+	string_result_tuple = initial_string
+	
+	#iterates through RESPONSE_LIST
+	for tuple in result_tuple[RESPONSE_LIST]:
+
+		timestamp_tuple = tuple[TIMESTAMP_INDEX]
+		timestamp_str = str(timestamp_tuple[TIMESTAMP_STR_INDEX])
+		
+		if timestamp_tuple[TIMESTAMP_BOOL_INDEX]:
+			timestamp_str = negative(timestamp_str)
+	
+		string_result_tuple += timestamp_str
+		string_result_tuple +="\n"
+		
+		#no alignment needed for the RESPONSE
+		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
+		string_result_tuple += (textwrap.fill(str(tuple[RESPONSE_INDEX]), 80))
+		string_result_tuple += "\n \n"
+		
+	return string_result_tuple
