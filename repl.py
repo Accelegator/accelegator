aea195a5caca3383cbc20a5857fd67265bb79bfa
Jerfenson Cerda Mejia <cerdamejiaj@aldenv168.allegheny.edu>
2017-10-30 20:56:32 -0400

    calling queries to the repl clasS

--- repl.py
+++ repl.py
@@ -1,13 +1,20 @@
+import queries
 def repl(command, DATAFRAME, key1, key2):
 
     if command == "list":
-        print("list")
+        queries.query_list(dataframe)
 
     if command == "show":
-        print("show")
+        if key2 == "":
+            queries.query_show(dataframe, key1)
+        else:
+            queries.query_show_field(dataframe, key1, key2)
 
     if command == "search":
-        print("search")
+        if key2 == "":
+            queries.query_search(dataframe, key1)
+        else:
+            queries.query_search_field(dataframe, key1, key2)
 
-    if command == "quit":
-        print("print")
+    if command == "latest":
+        queries.determine_latest(dataframe, key1, key3, key4 )
