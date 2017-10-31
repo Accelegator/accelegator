f18fe37e3c2055a8b08c211d6ce17e3143b9dbc1
Karol Vargas <vargask@aldenv105.allegheny.edu>
2017-10-31 13:26:14 -0400

    Uploading re-edit of display help function

--- display_help.py
+++ display_help.py
@@ -2,15 +2,15 @@
 
 # Implementation will be moved to display.py once completed
 
-def display_help(help):
+def display_help(display_help_with_command):
 
-    return  "{:<40s}{:<48s}".format('Commands, descriptions');
-            "{:<40s}{:<48s}".format('quit, exit the program');
-            "{:<40s}{:<48s}".format('show <email>, display flattened responses for advisee with <email>');
-            "{:<40s}{:<48s}".format('help <command>, verbose descriptions of commands & args');
-            "{:<40s}{:<48s}".format('help, list commands & brief descriptions');
-            "{:<40s}{:<48s}".format('show <email> <field>, display responses for given field, incl. history');
-            "{:<40s}{:<48s}".format('list, list advisees by emails & shows latest response date');
-            "{:<40s}{:<48s}".format('search <keyword>, search all fields of all responses of all students for keyword');
-            "{:<40s}{:<48s}".format('search <field> <keyword>, search given of all responses of all students for keyword');
-            "{:<40s}{:<48s}".format('write command, write command output to file(prompt for filepath)');
+    return  "{:<40s}{:<48s}".format("Commands", "descriptions")+"\n"+
+    "{:<40s}{:<48s}".format("quit" , "exit the program")+"\n"+
+    "{:<40s}{:<48s}".format("show <email>" , "display flattened responses for advisee with <email>")+"\n"+
+    "{:<40s}{:<48s}".format("help <command>" , "verbose descriptions of commands & args")+"\n"+
+    "{:<40s}{:<48s}".format("help" , "list commands & brief descriptions")+"\n"+
+    "{:<40s}{:<48s}".format("show <email> <field>" , "display responses for given field, incl. history")+"\n"+
+    "{:<40s}{:<48s}".format("list" , "list advisees by emails & shows latest response date")+"\n"+
+    "{:<40s}{:<48s}".format("search <keyword>" , "search all fields of all responses of all students for keyword")+"\n"+
+    "{:<40s}{:<48s}".format("search <field> <keyword>" , "search given of all responses of all students for keyword")+"\n"+
+    "{:<40s}{:<48s}".format("write command" , "write command output to file(prompt for filepath)")+"\n"
