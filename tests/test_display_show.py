5fe631fb63dad1414e3299b95da11f1b8d0c2e18
lylet-AC <lylet@allegheny.edu>
2017-10-30 21:27:16 -0400

    added display_show_with_field

--- tests/test_display_show.py
+++ tests/test_display_show.py
@@ -1,12 +1,12 @@
 from display_show import display_show
 from display_show import align
+from colors import bold
 
 def test_display_show():
 	"""checks that display_show will properly output information"""
-	tuple_of_tuples = (("Aug 03 00:00:35 2010", "email", "honokak@otonokizaka.edu"), 
-	("Apr 19 00:00:35 2010", "email", "nishikinom@otonokizaka.edu"),
-	("Jul 22 00:00:35 2010", "email", "yazawan@otonokizaka.edu"))
+	tuple_of_tuples = ("Aug 03 00:00:35 2010", "email", "honokak@otonokizaka.edu")
 	string_of_tuples = display_show(tuple_of_tuples)
+	assert string_of_tuples = "Showing all results for: \x1b[1mhonokak@otonokizaka.edu\x1b[0m" + "\n\n" + align("email", "Aug 03 00:00:35 2010") + "\n" + textwrap.fill("honokak@otonokizaka.edu"), 80)
 	
 def test_align():
     """ Checks that align() returns properly aligned string """
