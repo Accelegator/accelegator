f551b90127288f20352fc2aa322f2a1897a906a7
lylet-AC <lylet@allegheny.edu>
2017-10-31 15:26:20 -0400

    fixed display_show for new input type

--- display_show.py
+++ display_show.py
@@ -3,54 +3,56 @@ import textwrap
 import logging
 from display_search import align
 from colors import bold
+from colors import negative
 
 
-def display_show(tuple_of_tuples, has_field = False):
+def display_show(result_tuple):
 
 
 	#immediately leaves if the output is None
-	if not tuple_of_tuples:
+	if not result_tuple:
 		logging.error("No input was found for tuple_of_tuples")
 		return "No responses to display"
-
+		
+	#for display_show
+	"""`show <email>` --- `(email, [((timestamp, latest), field, response)])`"""
+	
 	#CONSTANTS
-	FIELD = 1
-	TIMESTAMP = tuple_of_tuples[2][0][0]
-	RESPONSE = tuple_of_tuples[2][1]
-	EMAIL_INDEX = 2
-	
-	FIRST_RESPONSE = tuple_of_tuples[2][1]
-	EMAIL_FIELD = tuple_of_tuples[0]
-	FIELD_TYPE = bold(FIRST_RESPONSE[FIELD])
-	
+	EMAIL_INDEX = 0
+	RESPONSE_LIST = 1
+	FIELD_INDEX = 1 
+	RESPONSE_INDEX = 2
+	TIMESTAMP_INDEX = 0
+	TIMESTAMP_STR_INDEX = 0
+	TIMESTAMP_BOOL_INDEX = 1
+
 	field_str = ""
 	timestamp_str= ""
 	string_tuple_of_tuples = ""
 	
-	if has_field:
-		initial_string = textwrap.fill("Displaying " + EMAIL_FIELD + "'s results for: " + FIELD_TYPE, 80)
-		initial_string += "\n\n"
-	else:
-		initial_string = textwrap.fill("Showing all results for: " + EMAIL_FIELD, 80)
-		initial_string += "\n\n"
+	initial_string = textwrap.fill("Showing all results for: " + str(result_tuple[EMAIL_INDEX]), 80)
+	initial_string += "\n\n"
 	
 	string_tuple_of_tuples = initial_string
 	
 	"""Message for code inheriter"""
-	#TODO: Change logic to match new input format:  (email, field, [((timestamp, latest), response)])
-	#Reference: Old input looked like this (("timestamp", "field", "response"), ("timestamp", "field", "response")
+	#TODO: iterate through response list
 	
-	for tuple in tuple_of_tuples:
+	for tuple in result_tuple[RESPONSE_LIST]:
 		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
-		field_str = (str(tuple[FIELD]))
-		timestamp_str = (str(tuple[TIMESTAMP]))
+		field_str = (str(tuple[FIELD_INDEX]))
+		timestamp_tuple = tuple[TIMESTAMP_INDEX]
+		timestamp_str = str(timestamp_tuple[TIMESTAMP_STR_INDEX])
+		
+		if timestamp_tuple[TIMESTAMP_BOOL_INDEX]:
+			negative(timestamp_str)
 		
 		#align function will align text to the right and left side of the terminal
-		string_tuple_of_tuples += align(field_str, timestamp_str)
-		string_tuple_of_tuples+="\n"
+		string_result_tuple += align(field_str, timestamp_str)
+		string_result_tuple +="\n"
 		
 		#no alignment needed for the RESPONSE
-		string_tuple_of_tuples+=(textwrap.fill(str(tuple[RESPONSE]), 80))
-		string_tuple_of_tuples+="\n \n"
+		string_result_tuple += (textwrap.fill(str(tuple[RESPONSE_INDEX]), 80))
+		string_result_tuple += "\n \n"
 		
-	return string_tuple_of_tuples
+	return string_result_tuple
