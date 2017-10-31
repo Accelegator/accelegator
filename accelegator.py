f593a50dcd0ce45b158baee63f88ea33aef0bdfa
Karol Vargas <vargask@aldenv105.allegheny.edu>
2017-10-31 13:30:58 -0400

    reuploaded

--- accelegator.py
+++ accelegator.py
@@ -4,9 +4,12 @@ import logging
 
 # local dependencies
 import display_strings
+import display_help_with_command
+import display_help
 from parse_arguments import parse_arguments
 
 if __name__ == '__main__':
 
     arguments = parse_arguments(sys.argv[1:])
     print(display_strings.WELCOME)
+    print(display_help_with_command.display_help_with_command("quit"))
