449b27ceed64d8280dbd626754e077f0fb000f70
lylet-AC <lylet@allegheny.edu>
2017-10-30 21:59:10 -0400

    finished testcase for test_display_show

--- tests/test_display_show.py
+++ tests/test_display_show.py
@@ -1,12 +1,12 @@
 from display_show import display_show
-from display_show import align
-from colors import bold
+from display_search import align
+
 
 def test_display_show():
 	"""checks that display_show will properly output information"""
 	tuple_of_tuples = ("Aug 03 00:00:35 2010", "email", "honokak@otonokizaka.edu")
 	string_of_tuples = display_show(tuple_of_tuples)
-	assert string_of_tuples = "Showing all results for: \x1b[1mhonokak@otonokizaka.edu\x1b[0m" + "\n\n" + align("email", "Aug 03 00:00:35 2010") + "\n" + textwrap.fill("honokak@otonokizaka.edu"), 80)
+	assert repr(string_of_tuples) == repr("Showing all results for: " +  bold("honokak@otonokizaka.edu") + "\n\n" + align("email", "Aug 03 00:00:35 2010") + "\n" + "honokak@otonokizaka.edu")*/
 	
 def test_align():
     """ Checks that align() returns properly aligned string """
