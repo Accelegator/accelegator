2e8e69334069a967b7363985f32e0bfd07a9a965
Jacob Sutter <sutterj@allegheny.edu>
2017-10-31 23:11:57 -0400

    updated

--- repl.py
+++ repl.py
@@ -12,56 +12,66 @@ import write_to_file
 def repl(command, dataframe, key1, key2, key3):
 
     if command == "list":
-        print(display_list(queries.query_list(dataframe)))
-        logging.info("calling query and display for " + command)
+        print(display_list.display_list(queries.query_list(dataframe)))
+        logging.debug("calling query and display for " + command)
         return 0
 
     if command == "show":
         if key2 == "":
-            print(display_show(queries.query_show(dataframe, key1)))
-            logging.info("calling query and display for " + command + " with key " + key1)
+            print(display_show.display_show(queries.query_show(dataframe, key1)))
+            logging.debug("calling query and display for " + command + " with key " + key1)
             return 1
         else:
-            print(display_show_with_field(queries.query_show_field(dataframe, key1, key2)))
-            logging.info("calling query and display for " + command + " with keys " + key1 + ", " + key2)
+            print(display_show_with_field.display_show_with_field(queries.query_show_field(dataframe, key1, key2)))
+            logging.debug("calling query and display for " + command + " with keys " + key1 + ", " + key2)
             return 2
 
     if command == "search":
         if key2 == "":
-            print(display_search(queries.query_search(dataframe, key1)))
-            logging.info("calling query and display for " + command + " with key " + key1)
+            print(display_search.display_search(queries.query_search(dataframe, key1)))
+            logging.debug("calling query and display for " + command + " with key " + key1)
             return 3
         else:
-            print(display_search_with_field(queries.query_search_field(dataframe, key1, key2)))
-            logging.info("calling query and display for " + command + " with keys " + key1 + ", " + key2)
+            print(display_search_with_field.display_search_with_field(queries.query_search_field(dataframe, key1, key2)))
+            logging.debug("calling query and display for " + command + " with keys " + key1 + ", " + key2)
             return 4
 
     if command == "write":
 
         if key1 == "list":
+            logging.debug("calling write of " + key1 + " function")
             data = queries.query_list(dataframe)
+            return 7
 
         elif key1 == "show":
             if key3 == "":
+                logging.debug("calling write of " + key1 + " function with key " + key2)
                 data = queries.query_show(dataframe, key2)
+                return 8
             else:
+                logging.debug("calling write of " + key1 + " function with keys " + key2 + ", " + key3)
                 data = queries.query_show_field(dataframe, key2, key3)
+                return 9
 
         elif key1 == "search":
             if key3 == "":
+                logging.debug("calling write of " + key1 + " function with key " + key2)
                 data = queries.query_search(dataframe, key2)
+                return 10
             else:
+                logging.debug("calling write of " + key1 + " function with keys " + key2 + ", " + key3)
                 data = queries.query_search_field(dataframe, key2, key3)
+                return 11
 
         file_name = str(input('file name: '))
         write_to_file.write(data, file_name)
 
     if command == "help":
         if key1 == "":
-            print(display_help())
-            logging.info("calling query and display for " + command)
+            print(display_help.display_help())
+            logging.debug("calling query and display for " + command)
             return 5
         else:
-            print(display_help_with_command())
-            logging.info("calling query and display for " + command)
+            print(display_help_with_command.display_help_with_command())
+            logging.debug("calling query and display for " + command)
             return 6
--- tests/test_repl.py
+++ tests/test_repl.py
@@ -4,23 +4,23 @@ import repl
 
 def test_repl_1():
     """ Test list """
-    assert repl("list") == 0
+    assert repl("list", "data") == 0
 
 def test_repl_2():
     """ Test show with keyword """
-    assert repl("show", "email") == 1
+    assert repl("show", "data", "email") == 1
 
 def test_repl_3():
     """ Test show with two keywords """
-    assert repl("show", "email", "name") == 2
+    assert repl("show", "data", "email", "name") == 2
 
 def test_repl_4():
     """ Test search with keyword """
-    assert repl("search", "email") == 3
+    assert repl("search", "data", "email") == 3
 
 def test_repl_5():
     """ Test search with two keywords """
-    assert repl("search", "email", "name") == 4
+    assert repl("search", "data", "email", "name") == 4
 
 def test_repl_6():
     """ Test help """
@@ -29,3 +29,23 @@ def test_repl_6():
 def test_repl_7():
     """ Test help with command """
     assert repl("help", "command") == 6
+
+def test_repl_8():
+    """ Test write list """
+    assert repl("write", "data", "list") == 7
+
+def test_repl_9():
+    """ Test write show with keyword """
+    assert repl("write", "data", "show", "email") == 8
+
+def test_repl_10():
+    """ Test write show with two keywords """
+    assert repl("write", "data", "show", "email", "name") == 9
+
+def test_repl_11():
+    """ Test write search with keyword """
+    assert repl("write", "data", "search", "email") == 10
+
+def test_repl_12():
+    """ Test write search with two keywords """
+    assert repl("write", "data", "search", "email", "name") == 11
