f89c66964ed37369839be9d913f30f8d292c6281
yeeunmariakim <yeeunmariakim@gmail.com>
2017-11-01 19:15:26 -0400

    refactoring confusing code

--- display_show.py
+++ display_show.py
@@ -1,56 +1,37 @@
-""" Displays an advisee's flattened responses. """
+""" Returns string with an advisee's flattened responses. """
 import textwrap
 import logging
-from display_search import align
+import display_search
 from colors import bold
 from colors import negative
 
 
 def display_show(result_tuple):
+    """ Returns string with an advisee's flattened responses. Input is in form: (email, [((timestamp, latest), field, response)]) """
 
+    if not result_tuple:
+        logging.error("No input tuple given")
+        return "No responses to display"
 
-	#immediately leaves if the output is None
-	if not result_tuple:
-		logging.error("No input was found for tuple_of_tuples")
-		return "No responses to display"
-		
-	#input for display_show
-	"""`show <email>` --- `(email, [((timestamp, latest), field, response)])`"""
-	
-	#CONSTANTS
-	EMAIL_INDEX = 0
-	RESPONSE_LIST = 1
-	FIELD_INDEX = 1
-	RESPONSE_INDEX = 2
-	TIMESTAMP_INDEX = 0
-	TIMESTAMP_STR_INDEX = 0
-	TIMESTAMP_BOOL_INDEX = 1
+    EMAIL_INDEX = 0
+    RESPONSE_LIST_INDEX = 1
 
-	field_str = ""
-	timestamp_str= ""
-	string_result_tuple = ""
-	
-	initial_string = textwrap.fill("Showing all results for: " + str(result_tuple[EMAIL_INDEX]), 80)
-	initial_string += "\n\n"
-	
-	string_tuple_of_tuples = initial_string
-	
-	#iterates through RESPONSE_LIST
-	for tuple in result_tuple[RESPONSE_LIST]:
-		field_str = (str(tuple[FIELD_INDEX]))
-		timestamp_tuple = tuple[TIMESTAMP_INDEX]
-		timestamp_str = str(timestamp_tuple[TIMESTAMP_STR_INDEX])
-		
-		if timestamp_tuple[TIMESTAMP_BOOL_INDEX]:
-			timestamp_str = negative(timestamp_str)
-		
-		#align function will align text to the right and left side of the terminal
-		string_result_tuple += align(field_str, timestamp_str)
-		string_result_tuple +="\n"
-		
-		#no alignment needed for the RESPONSE
-		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.		
-		string_result_tuple += (textwrap.fill(str(tuple[RESPONSE_INDEX]), 80))
-		string_result_tuple += "\n \n"
-		
-	return string_result_tuple
+    TIMESTAMP_INDEX = 0
+    TIMESTAMP_STR_INDEX = 0
+    TIMESTAMP_LATEST_INDEX = 1
+    FIELD_INDEX = 1
+    RESPONSE_INDEX = 2
+
+    result = textwrap.fill("Showing flattened responses for advisee with email " + bold(str(result_tuple[EMAIL_INDEX])), 80) + "\n"
+
+    for response in result_tuple[RESPONSE_LIST_INDEX]:
+        field = (str(response[FIELD_INDEX]))
+        timestamp_tuple = response[TIMESTAMP_INDEX]
+        timestamp_latest = timestamp_tuple[TIMESTAMP_LATEST_INDEX]
+        timestamp = negative(str(timestamp_tuple[TIMESTAMP_STR_INDEX])) if timestamp_latest else str(timestamp_tuple[TIMESTAMP_STR_INDEX])
+        response_string = str(response[RESPONSE_INDEX])
+        result += display_search.align(field, timestamp) + "\n"
+
+        result += textwrap.fill(response_string, width=80) + "\n"
+
+    return result
