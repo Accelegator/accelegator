6364f4cf70bb6b50affe28c3eb118ce9ca324f7d
yeeunmariakim <yeeunmariakim@gmail.com>
2017-11-01 19:23:00 -0400

    changing comment

--- display_show_with_field.py
+++ display_show_with_field.py
@@ -1,54 +1,33 @@
-""" Displays an advisee's flattened responses. """
+""" Returns a string with an advisee's past and current responses to a given field. """
 import textwrap
 import logging
 from colors import bold
 from colors import negative
 
 
-def display_show_with_field(tuple_of_tuples):
-	""" Displays an advisee's flattened responses. """
-	
-	#immediately leaves if the output is None
-	if not tuple_of_tuples:
-		logging.error("No input was found for tuple_of_tuples")
-		return "No responses to display"
-		
-	#input for display_show_with_feild
-	"""`show <email> <field>` --- `(email, field, [((timestamp, latest), response)])`"""
-	
-	#CONSTANTS
-	EMAIL_INDEX = 0
-	RESPONSE_LIST = 2
-	FIELD_INDEX = 1 
-	RESPONSE_INDEX = 1
-	TIMESTAMP_INDEX = 0
-	TIMESTAMP_STR_INDEX = 0
-	TIMESTAMP_BOOL_INDEX = 1
+def display_show_with_field(result_tuple):
+    """ Returns a string with an advisee's past and current responses to a given field. Input is in form: (email, field, [((timestamp, latest), response)])"""
 
-	field_str = ""
-	timestamp_str= ""
-	string_tuple_of_tuples = ""
-	
-	initial_string = textwrap.fill("Displaying " + result_tuple[EMAIL_INDEX] + "'s results for: " + result_tuple[FIELD_INDEX], 80)
-	initial_string += "\n\n"
-	
-	string_result_tuple = initial_string
-	
-	#iterates through RESPONSE_LIST
-	for tuple in result_tuple[RESPONSE_LIST]:
+    if not result_tuple:
+        logging.error("No input tuple given")
+        return "No responses to display"
 
-		timestamp_tuple = tuple[TIMESTAMP_INDEX]
-		timestamp_str = str(timestamp_tuple[TIMESTAMP_STR_INDEX])
-		
-		if timestamp_tuple[TIMESTAMP_BOOL_INDEX]:
-			timestamp_str = negative(timestamp_str)
-	
-		string_result_tuple += timestamp_str
-		string_result_tuple +="\n"
-		
-		#no alignment needed for the RESPONSE
-		#textwrap.fill(<data>, 80) will wrap the text in lines of 80 characters.
-		string_result_tuple += (textwrap.fill(str(tuple[RESPONSE_INDEX]), 80))
-		string_result_tuple += "\n \n"
-		
-	return string_result_tuple
+    EMAIL_INDEX = 0
+    FIELD_INDEX = 1
+    RESPONSE_LIST_INDEX = 2
+
+    TIMESTAMP_INDEX = 0
+    TIMESTAMP_STR_INDEX = 0
+    TIMESTAMP_LATEST_INDEX = 1
+    RESPONSE_INDEX = 1
+
+    result = textwrap.fill("Showing responses for advisee with email " + bold(str(result_tuple[EMAIL_INDEX])) + " to field " + bold(str(result_tuple[FIELD_INDEX])), 80) + "\n\n"
+
+    for response in result_tuple[RESPONSE_LIST_INDEX]:
+        timestamp_tuple = response[TIMESTAMP_INDEX]
+        timestamp_latest = timestamp_tuple[TIMESTAMP_LATEST_INDEX]
+        timestamp = negative(str(timestamp_tuple[TIMESTAMP_STR_INDEX])) if timestamp_latest else str(timestamp_tuple[TIMESTAMP_STR_INDEX])
+        response_string = str(response[RESPONSE_INDEX])
+        result += timestamp + "\n" + textwrap.fill(response_string, width=80) + "\n"
+
+    return result
